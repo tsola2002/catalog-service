1264a07441a02e47602612a12aedddaf6d315805
Omatsola Isaac Sobotie <tsola2002@yahoo.co.uk>
2022-12-05 06:55:28 -0600

    new spring kotlin catalog project

---
+++ src/main/java/com/kotlinspring/CourseCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kotlinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseCatalogServiceApplication.class, args);
+	}
+
+}
